68b88fe0c0168a66622c674676de11fa20a2ab75
flomess <florian.messu@gmail.com>
2021-04-15 13:06:48 +0200

    redirect registrer

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -58,7 +58,7 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-
+            return $this->redirectToRoute('accueil');
 
         }
 
